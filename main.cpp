37a506e48626f21754edb6dcb325efa09ebfe85c
noahbei <nbeidelman9@gmail.com>
2024-10-27 23:34:51 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//============================================================
+// file: main.cpp
+//============================================================
+#include <iostream>
+#include <cstring>
+#include <initializer_list>
+#include <cassert>
+
+//MATRIX and VECTOR classes assignment
+#include "vector3d.h"
+#include "matrix3d.h"
+#include "quaternion.h"
+
+int main(int argc, const char * argv[]) {
+//   vector3D::run_tests();
+//   matrix3D::run_tests();
+  quaternionD::run_tests();
+
+    
+  return 0;
+}
+
+
