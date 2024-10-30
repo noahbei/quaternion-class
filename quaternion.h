9c0249d344691023cee5b0fc5d338117fd329322
noahbei <nbeidelman9@gmail.com>
2024-10-29 20:09:19 -0700

    finish quaternion code -- all tests pass

--- quaternion.h
+++ quaternion.h
@@ -41,7 +41,13 @@ public:
 
   friend quaternion operator+(const quaternion& a, const quaternion& b) { return quaternion(a.w + b.w, a.x + b.x, a.y + b.y, a.z + b.z); }
   friend quaternion operator-(const quaternion& a, const quaternion& b) { return quaternion(a.w - b.w, a.x - b.x, a.y - b.y, a.z - b.z); }
-  friend quaternion operator*(const quaternion& a, const quaternion& b);
+  friend quaternion operator*(const quaternion& a, const quaternion& b) { 
+    return quaternion(a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z,
+                      a.w * b.x + a.x * b.w + a.y * b.z - a.z * b.y,
+                      a.w * b.y - a.x * b.z + a.y * b.w + a.z * b.x,
+                      a.w * b.z + a.x * b.y - a.y * b.x + a.z * b.w
+                      );
+  }
 
   friend quaternion operator+(const quaternion& q, T k) { return quaternion(q.w + k, q.x, q.y, q.z); }
   friend quaternion operator+(T k, const quaternion& q) { return quaternion(q.w + k, q.x, q.y, q.z); }
@@ -49,11 +55,11 @@ public:
   friend quaternion operator-(const quaternion& q, T k) { return quaternion(q.w - k, q.x, q.y, q.z); }
   friend quaternion operator-(T k, const quaternion& q) { return quaternion(q.w - k, q.x, q.y, q.z); }
 
-  friend quaternion operator*(const quaternion& q, T k);
-  friend quaternion operator*(T k, const quaternion& q);
-  friend quaternion operator/(const quaternion& q, T k);
+  friend quaternion operator*(const quaternion& q, T k) { return quaternion(q.w * k, q.x * k, q.y * k, q.z * k); }
+  friend quaternion operator*(T k, const quaternion& q) { return quaternion(q.w * k, q.x * k, q.y * k, q.z * k); }
+  friend quaternion operator/(const quaternion& q, T k) { return quaternion(q.w / k, q.x / k, q.y / k, q.z / k); }
 
-  quaternion operator-() const;
+  quaternion operator-() const { return quaternion(w * -1, x * -1, y * -1, z * -1); }
 
   friend bool operator==(const quaternion& q, const quaternion& r) { return q.w == r.w && q.x == r.x && q.y == r.y && q.z == r.z; }
   friend bool operator!=(const quaternion& q, const quaternion& r) { return !(q == r); }
@@ -63,7 +69,7 @@ public:
 
   quaternion unit_scalar() const;
 
-  quaternion conjugate() const;
+  quaternion conjugate() const { return quaternion(w, x * -1, y * -1, z * -1); }
 
   // void check_divide_by_zero(double magn, char* x) {
   //   if (magn == 0)
@@ -77,7 +83,7 @@ public:
 
   quaternion unit() const {
     double k = magnitude();
-    check_divide_by_zero(k);
+    //check_divide_by_zero(k);
     return *this / k;
   }
 
@@ -156,105 +162,105 @@ void quaternion<T>::run_tests() {
   assert(a * 3 == quatD(a.w * 3, a.x * 3, a.y * 3, a.z * 3));
   assert(a * 3 != quatD(a.w * 3, a.x, a.y, a.z));
 
-  // std::cout << "a = " << a << ")\nb = " << b << ")\nc = " << c << ")\nd = " << d
-  //           << ")\ne = " << e << ")\nf = " << f << ")\ng = " << g << ")\nh = " <<  h << "\n";
+  std::cout << "a = " << a << ")\nb = " << b << ")\nc = " << c << ")\nd = " << d
+            << ")\ne = " << e << ")\nf = " << f << ")\ng = " << g << ")\nh = " <<  h << "\n";
 
-  // std::cout << "c + d = " <<  c + d << "\nc + d + e = " << c + d + e << "\n";
-  // std::cout << "5 * h = " << 5 * h << "\nh * 5 = " << h * 5 << "\nh / 3.0 = " << h / 3.0 << "\n\n";
+  std::cout << "c + d = " <<  c + d << "\nc + d + e = " << c + d + e << "\n";
+  std::cout << "5 * h = " << 5 * h << "\nh * 5 = " << h * 5 << "\nh / 3.0 = " << h / 3.0 << "\n\n";
 
-  // std::cout << "h.magnitude() is " << h.mag() << "\nh.unit() is " << h.unit() << "\n";
-  // std::cout << "g.unit() is " << g.unit() << "\na.unit() is " << a.unit() << ")\n\n";
+  std::cout << "h.magnitude() is " << h.mag() << "\nh.unit() is " << h.unit() << "\n";
+  std::cout << "g.unit() is " << g.unit() << "\na.unit() is " << a.unit() << ")\n\n";
 
-  // std::cout << "a.vector() is " << a.vector() << "\na.scalar() is " << a.scalar() << "\n";
-  // std::cout << "a.conjugate() is " << a.conjugate() << "\na.inverse() is " << a.inverse()
-  //           << "\na * a.inverse() is " << a * a.inverse() << "\n\n";
+  std::cout << "a.vector() is " << a.vector() << "\na.scalar() is " << a.scalar() << "\n";
+  std::cout << "a.conjugate() is " << a.conjugate() << "\na.inverse() is " << a.inverse()
+            << "\na * a.inverse() is " << a * a.inverse() << "\n\n";
 
-  // std::cout << "c == d is " << yes_or_no(c == d) << "\nc != d is " << yes_or_no(c != d);
-  // std::cout << "\ne == e is " << yes_or_no(e == e) << "\ne != e is " << yes_or_no(e != e) << "\n";
+  std::cout << "c == d is " << yes_or_no(c == d) << "\nc != d is " << yes_or_no(c != d);
+  std::cout << "\ne == e is " << yes_or_no(e == e) << "\ne != e is " << yes_or_no(e != e) << "\n";
 
-  // std::cout << "\n\nquat.ij is: " << quatD::ij() << "\nquat.jk is: " << quatD::jk()
-  //           << "\nquat.ki is: " << quatD::ki() << "\n";
-  // assert(quatD::ij() == quatD::k());
-  // assert(quatD::jk() == quatD::i());
-  // assert(quatD::ki() == quatD::j());
+  std::cout << "\n\nquat.ij is: " << quatD::ij() << "\nquat.jk is: " << quatD::jk()
+            << "\nquat.ki is: " << quatD::ki() << "\n";
+  assert(quatD::ij() == quatD::k());
+  assert(quatD::jk() == quatD::i());
+  assert(quatD::ki() == quatD::j());
 
-  // std::cout << "\nquat.ji is: " << quatD::ji() << "\nquat.kj is: " << quatD::kj()
-  //           << "\nquat.ik is: " << quatD::ik() << "\nquat.ijk is: " << quatD::ijk() << "\n";
-  // assert(quatD::ji() == -quatD::k());
-  // assert(quatD::kj() == -quatD::i());
-  // assert(quatD::ik() == -quatD::j());
+  std::cout << "\nquat.ji is: " << quatD::ji() << "\nquat.kj is: " << quatD::kj()
+            << "\nquat.ik is: " << quatD::ik() << "\nquat.ijk is: " << quatD::ijk() << "\n";
+  assert(quatD::ji() == -quatD::k());
+  assert(quatD::kj() == -quatD::i());
+  assert(quatD::ik() == -quatD::j());
 
-  // std::cout << "\nquat.ii is: " << quatD::ii() << "\nquat.jj is: " << quatD::jj()
-  //           << "\nquat.kk is: " << quatD::kk() << "\n";
-  // assert(quatD::ii()  == -1);
-  // assert(quatD::jj()  == -1);
-  // assert(quatD::kk()  == -1);
-  // assert(quatD::ijk() == -1);
+  std::cout << "\nquat.ii is: " << quatD::ii() << "\nquat.jj is: " << quatD::jj()
+            << "\nquat.kk is: " << quatD::kk() << "\n";
+  assert(quatD::ii()  == -1);
+  assert(quatD::jj()  == -1);
+  assert(quatD::kk()  == -1);
+  assert(quatD::ijk() == -1);
 
-  // std::cout << "\nangle (deg) between c and d is: " << c.angle(d) << "\n";
-  // quatD c_minus_d = c - d;
-  // std::cout << "c_minus_d is: " << c_minus_d;
-  // matrix3D rot_matrix = c_minus_d.rot_matrix();
-  // std::cout << "rot_matrix of c_minus_d is: " << c_minus_d.rot_matrix() << "\n";
+  std::cout << "\nangle (deg) between c and d is: " << c.angle(d) << "\n";
+  quatD c_minus_d = c - d;
+  std::cout << "c_minus_d is: " << c_minus_d;
+  matrix3D rot_matrix = c_minus_d.rot_matrix();
+  std::cout << "rot_matrix of c_minus_d is: " << c_minus_d.rot_matrix() << "\n";
 
-  // double rad2_2 = sqrt(2)/2.0;
-  // std::cout << "// -------------- LEVEL FLIGHT -------------------')\n";
-  // plane_rotation("levelFlight(E)", quatD(1,      0, 0,       0), {  1,  0,  0,   0,  1,  0,   0,  0,  1 });
-  // plane_rotation("levelFlight(N)", quatD(rad2_2, 0, rad2_2,  0), {  0,  0,  1,   0,  1,  0,  -1,  0,  0 });
-  // plane_rotation("levelFlight(W)", quatD(0,      0,  1,      0), { -1,  0,  0,   0,  1,  0,   0,  0, -1 });
-  // plane_rotation("levelFlight(S)", quatD(rad2_2, 0, -rad2_2, 0), {  0,  0, -1,   0,  1,  0,   1,  0,  0} );
-  // std::cout << "LEVEL FLIGHT assertions passed ..............................................\n";
-  // std::cout << "// --------- end LEVEL FLIGHT ------------------------)\n\n";
+  double rad2_2 = sqrt(2)/2.0;
+  std::cout << "// -------------- LEVEL FLIGHT -------------------')\n";
+  plane_rotation("levelFlight(E)", quatD(1,      0, 0,       0), {  1,  0,  0,   0,  1,  0,   0,  0,  1 });
+  plane_rotation("levelFlight(N)", quatD(rad2_2, 0, rad2_2,  0), {  0,  0,  1,   0,  1,  0,  -1,  0,  0 });
+  plane_rotation("levelFlight(W)", quatD(0,      0,  1,      0), { -1,  0,  0,   0,  1,  0,   0,  0, -1 });
+  plane_rotation("levelFlight(S)", quatD(rad2_2, 0, -rad2_2, 0), {  0,  0, -1,   0,  1,  0,   1,  0,  0} );
+  std::cout << "LEVEL FLIGHT assertions passed ..............................................\n";
+  std::cout << "// --------- end LEVEL FLIGHT ------------------------)\n\n";
 
-  // std::cout << "// -------------- STRAIGHT UP -------------------')\n";
-  // plane_rotation("straightUp(E)", quatD(rad2_2, 0, 0, rad2_2),   {  0, -1,  0,   1,  0,  0,   0,  0,  1 } );
-  // plane_rotation("straightUp(N)", quatD(0.5, 0.5, 0.5, 0.5),     {  0,  0,  1,   1,  0,  0,   0,  1,  0 } );
-  // plane_rotation("straightUp(W)", quatD(0, rad2_2, rad2_2, 0),   {  0,  1,  0,   1,  0,  0,   0,  0, -1 } );
-  // plane_rotation("straightUp(S)", quatD(0.5, -0.5, -0.5, 0.5),   {  0,  0, -1,   1,  0,  0,   0, -1,  0 } );
-  // std::cout << "STRAIGHT UP assertions passed..............................................\n";
-  // std::cout << "// --------- end STRAIGHT UP ------------------------)\n\n";
-
-
-  // std::cout << "// -------------- STRAIGHT DOWN ------------------')\n";
-  // plane_rotation("straightDown(E)", quatD(rad2_2, 0, 0, -rad2_2), {  0,  1,  0,  -1,  0,  0,   0,  0,  1 } );
-  // plane_rotation("straightDown(N)", quatD(0.5, -0.5, 0.5, -0.5),  {  0,  0,  1,  -1,  0,  0,   0, -1,  0 });
-  // plane_rotation("straightDown(N)", quatD(0, -rad2_2, rad2_2, 0), {  0, -1,  0,  -1,  0,  0,   0,  0, -1 } );
-  // plane_rotation("straightDown(S)", quatD(0.5, 0.5, -0.5, -0.5),  {  0,  0, -1,  -1,  0,  0,   0,  1,  0 });
-  // std::cout << "STRAIGHT DOWN assertions passed..............................................\n";
-  // std::cout << "// --------- end STRAIGHT DOWN ----------------------)\n\n";
+  std::cout << "// -------------- STRAIGHT UP -------------------')\n";
+  plane_rotation("straightUp(E)", quatD(rad2_2, 0, 0, rad2_2),   {  0, -1,  0,   1,  0,  0,   0,  0,  1 } );
+  plane_rotation("straightUp(N)", quatD(0.5, 0.5, 0.5, 0.5),     {  0,  0,  1,   1,  0,  0,   0,  1,  0 } );
+  plane_rotation("straightUp(W)", quatD(0, rad2_2, rad2_2, 0),   {  0,  1,  0,   1,  0,  0,   0,  0, -1 } );
+  plane_rotation("straightUp(S)", quatD(0.5, -0.5, -0.5, 0.5),   {  0,  0, -1,   1,  0,  0,   0, -1,  0 } );
+  std::cout << "STRAIGHT UP assertions passed..............................................\n";
+  std::cout << "// --------- end STRAIGHT UP ------------------------)\n\n";
 
 
-  // std::cout << "\n\n -------- BANK/ROLL ----------------\n";
-  // std::cout << "\nBanking/Rolling 90 degrees left...\n";
-  // plane_rotation("plane_E_bankLeft90", quatD(rad2_2, rad2_2, 0, 0),  {  1,  0,  0,   0,  0, -1,   0,  1,  0 } );
-  // plane_rotation("plane_N_bankLeft90", quatD(0.5, 0.5, 0.5, -0.5),   {  0,  1,  0,   0,  0, -1,  -1,  0,  0 } );
-  // plane_rotation("plane_W_bankLeft90", quatD(0, 0, rad2_2, -rad2_2), { -1,  0,  0,   0,  0, -1,   0, -1,  0 }  );
-  // plane_rotation("plane_W_bankLeft90", quatD(0.5, 0.5, -0.5, 0.5),   {  0, -1,  0,   0,  0, -1,   1,  0,  0 } );
-  // std::cout << "ROLL 90 deg left assertions passed..............................................\n";
+  std::cout << "// -------------- STRAIGHT DOWN ------------------')\n";
+  plane_rotation("straightDown(E)", quatD(rad2_2, 0, 0, -rad2_2), {  0,  1,  0,  -1,  0,  0,   0,  0,  1 } );
+  plane_rotation("straightDown(N)", quatD(0.5, -0.5, 0.5, -0.5),  {  0,  0,  1,  -1,  0,  0,   0, -1,  0 });
+  plane_rotation("straightDown(N)", quatD(0, -rad2_2, rad2_2, 0), {  0, -1,  0,  -1,  0,  0,   0,  0, -1 } );
+  plane_rotation("straightDown(S)", quatD(0.5, 0.5, -0.5, -0.5),  {  0,  0, -1,  -1,  0,  0,   0,  1,  0 });
+  std::cout << "STRAIGHT DOWN assertions passed..............................................\n";
+  std::cout << "// --------- end STRAIGHT DOWN ----------------------)\n\n";
 
-  // std::cout << "\n\nBanking/Rolling 180 degrees...\n";
-  // plane_rotation("plane_E_bankLeft180", quatD(0, 1, 0, 0),            {  1,  0,  0,   0, -1,  0,   0,  0, -1 });
-  // plane_rotation("plane_N_bankLeft180", quatD(0, rad2_2, 0, -rad2_2), {  0,  0, -1,   0, -1,  0,  -1,  0,  0 });
-  // plane_rotation("plane_W_bankLeft180", quatD(0, 0, 0, 1),            { -1,  0,  0,   0, -1,  0,   0,  0,  1 });
-  // plane_rotation("plane_S_bankLeft180", quatD(0, rad2_2, 0, rad2_2),  {  0,  0,  1,   0, -1,  0,   1,  0,  0 });
-  // std::cout << "ROLL 180 degrees assertions passed..............................................\n";
 
-  // std::cout << "\n\nBanking/Rolling 90 degrees right...\n";
-  // plane_rotation("plane_E_bankRight90", quatD(rad2_2, -rad2_2, 0, 0), {  1,  0,  0,   0,  0,  1,   0, -1,  0 });
-  // plane_rotation("plane_N_bankRight90", quatD(0.5, -0.5, 0.5, 0.5),   {  0, -1,  0,   0,  0,  1,  -1,  0,  0 });
-  // plane_rotation("plane_W_bankRight90", quatD(0, 0, rad2_2, rad2_2),  { -1,  0,  0,   0,  0,  1,   0,  1,  0 });
-  // plane_rotation("plane_S_bankRight90", quatD(0.5, -0.5, -0.5, -0.5), {  0,  1,  0,   0,  0,  1,   1,  0,  0 });
-  // std::cout << "ROLL 90 deg right assertions passed..............................................\n";
-  // std::cout << "\n -------- end BANK/ROLL ----------------\n";
+  std::cout << "\n\n -------- BANK/ROLL ----------------\n";
+  std::cout << "\nBanking/Rolling 90 degrees left...\n";
+  plane_rotation("plane_E_bankLeft90", quatD(rad2_2, rad2_2, 0, 0),  {  1,  0,  0,   0,  0, -1,   0,  1,  0 } );
+  plane_rotation("plane_N_bankLeft90", quatD(0.5, 0.5, 0.5, -0.5),   {  0,  1,  0,   0,  0, -1,  -1,  0,  0 } );
+  plane_rotation("plane_W_bankLeft90", quatD(0, 0, rad2_2, -rad2_2), { -1,  0,  0,   0,  0, -1,   0, -1,  0 }  );
+  plane_rotation("plane_W_bankLeft90", quatD(0.5, 0.5, -0.5, 0.5),   {  0, -1,  0,   0,  0, -1,   1,  0,  0 } );
+  std::cout << "ROLL 90 deg left assertions passed..............................................\n";
 
-  // std::cout << "\nALL PLANE ROTATION ASSERTIONS PASSED ............................................\n\n";
+  std::cout << "\n\nBanking/Rolling 180 degrees...\n";
+  plane_rotation("plane_E_bankLeft180", quatD(0, 1, 0, 0),            {  1,  0,  0,   0, -1,  0,   0,  0, -1 });
+  plane_rotation("plane_N_bankLeft180", quatD(0, rad2_2, 0, -rad2_2), {  0,  0, -1,   0, -1,  0,  -1,  0,  0 });
+  plane_rotation("plane_W_bankLeft180", quatD(0, 0, 0, 1),            { -1,  0,  0,   0, -1,  0,   0,  0,  1 });
+  plane_rotation("plane_S_bankLeft180", quatD(0, rad2_2, 0, rad2_2),  {  0,  0,  1,   0, -1,  0,   1,  0,  0 });
+  std::cout << "ROLL 180 degrees assertions passed..............................................\n";
 
-  // std::cout << "SEE THIS WEBSITE for DETAILED DIAGRAMS on the TESTS of the PLANE's rotations\n";
-  // std::cout << "https://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/examples/index.htm\n";
+  std::cout << "\n\nBanking/Rolling 90 degrees right...\n";
+  plane_rotation("plane_E_bankRight90", quatD(rad2_2, -rad2_2, 0, 0), {  1,  0,  0,   0,  0,  1,   0, -1,  0 });
+  plane_rotation("plane_N_bankRight90", quatD(0.5, -0.5, 0.5, 0.5),   {  0, -1,  0,   0,  0,  1,  -1,  0,  0 });
+  plane_rotation("plane_W_bankRight90", quatD(0, 0, rad2_2, rad2_2),  { -1,  0,  0,   0,  0,  1,   0,  1,  0 });
+  plane_rotation("plane_S_bankRight90", quatD(0.5, -0.5, -0.5, -0.5), {  0,  1,  0,   0,  0,  1,   1,  0,  0 });
+  std::cout << "ROLL 90 deg right assertions passed..............................................\n";
+  std::cout << "\n -------- end BANK/ROLL ----------------\n";
+
+  std::cout << "\nALL PLANE ROTATION ASSERTIONS PASSED ............................................\n\n";
+
+  std::cout << "SEE THIS WEBSITE for DETAILED DIAGRAMS on the TESTS of the PLANE's rotations\n";
+  std::cout << "https://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToMatrix/examples/index.htm\n";
   
-  // std::cout << "\n...ALL QUATERNION ASSERTIONS PASSED!" << "\n";
+  std::cout << "\n...ALL QUATERNION ASSERTIONS PASSED!" << "\n";
 
-  // std::cout << "\n====================  FINISHED testing QUATERNIONS  ========================" << "\n\n";
+  std::cout << "\n====================  FINISHED testing QUATERNIONS  ========================" << "\n\n";
 }
 
 #endif /* quaternion_T_h */
